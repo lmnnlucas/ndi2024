18bede275a144bf36f75df128ce92ca5ccc9345e
Quentin Marrot <quentin.marrot@etu.u-bordeaux.fr>
2024-12-06 00:33:05 +0100

    Pop-up start

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PopupComponent } from '../../shared/pop-up/pop-up.component'; 
+import { JsonDataService } from '../../shared/json-data/json-data.service';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [PopupComponent],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+  styleUrls: ['./game.component.css']
 })
 export class GameComponent {
+  @ViewChild(PopupComponent) popup!: PopupComponent;
+  jsonContent: any;
 
+  constructor(private jsonService: JsonDataService) {}
+
+  openPopUp(file: string) {
+    this.jsonService.loadData(file).subscribe((data) => {
+      this.jsonContent = data; 
+      this.popup.openPopup();
+    });
+  }
 }
