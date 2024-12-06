46db2e5e11b6b9744f99c3b04aa8a4d5ee47035e
Yoann Gourdin <yoann.gourdin@etu.u-bordeaux.fr>
2024-12-06 04:02:47 +0100

    1v1 et IA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { GameComponent } from './pages/game/game.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './pages/card/card.component';
+import { VersusComponent } from './pages/versus/versus.component';
+import { IAComponent } from './pages/ia/ia.component';
 
 
 export const routes: Routes = [
     {path: 'jeu', component: GameComponent},
     {path: 'card', component: CardComponent},
+    {path: 'versus', component: VersusComponent},
+    {path: 'ia', component: IAComponent},
     {path: '**', component: HomeComponent}
 ];
